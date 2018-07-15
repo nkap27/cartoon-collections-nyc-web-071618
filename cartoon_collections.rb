0fcc014f3c3e45a0c21c79ad772333d412d10458
Natasha K <kapr.natasha@gmail.com>
2018-07-15 21:35:02 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,7 +22,9 @@ def long_planeteer_calls(words)
   end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(snacks)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  snacks.find do |item|
+    item.cheese_types?
+  end
 end
